dda9b641eb3994eb364e6cb00335cb017db6dd76
kallakish <49141155+kallakish@users.noreply.github.com>
2025-01-30 16:16:05 +0000

    Add files via upload

---
+++ audit/tables/CurrentExecution.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [audit].CurrentExecution
+(
+	CurrentExecutionId		UNIQUEIDENTIFIER	        NOT NULL	
+,	ApplicationId			SMALLINT		       	NOT NULL
+,	StageId				SMALLINT			NOT NULL
+,	SetId				    INT			NOT NULL	DEFAULT(1)
+,	PipelineId			    INT			NOT NULL
+,	CreatedDateTime		DATETIME2(0)		NULL	
+,	StartDateTime			DATETIME2(0)		        NULL		
+,	PipelineStatus			VARCHAR(50)			NULL
+,	EndDateTime			DATETIME2(0)			NULL
+,	RowsRead			INT				NULL
+,	RowsCopied			INT				NULL
+,	PipelineParameterUsed	        NVARCHAR(MAX)			NULL  
+,	ErrorMessage			NVARCHAR(1000)			NULL
+,	ADFRunId                        VARCHAR (50)        		NULL
+-- PK's
+,	CONSTRAINT PK_CurrentExecution				PRIMARY KEY CLUSTERED (CurrentExecutionId, ApplicationId, StageId, PipelineId)
+);
+GO
---
+++ audit/tables/HistoricalExecution.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [audit].HistoricalExecution
+(
+	HistoricalExecutionId	INT			NOT NULL	IDENTITY(1,1)
+,	CurrentExecutionId	UNIQUEIDENTIFIER	NOT NULL
+,	ApplicationId		SMALLINT		NOT NULL
+,	StageId			SMALLINT		NOT NULL
+,	SetId			     INT		NOT NULL	DEFAULT(1)
+,	PipelineId		     INT		NOT NULL
+,	CreatedDateTime		DATETIME2(0)		NULL	
+,	StartDateTime		DATETIME2(0)		NULL	
+,	PipelineStatus		VARCHAR(50)		NULL
+,	EndDateTime		DATETIME2(0)		NULL
+,	RowsRead		INT			NULL
+,	RowsCopied		INT			NULL
+,	PipelineParameterUsed	NVARCHAR(MAX)		NULL		CONSTRAINT df_hisexeParam	DEFAULT('none')
+,	ErrorMessage		NVARCHAR(1000)		NULL
+,	ADFRunId                VARCHAR (50)            NULL
+
+-- PK's
+,	CONSTRAINT PK_HistoricalExecution				PRIMARY KEY CLUSTERED (HistoricalExecutionId)
+);
+GO
